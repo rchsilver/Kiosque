13207437991605e0c43379e157856c659e82a956
Gael <rchsilver@gmail.com>
2023-05-23 16:59:55 -0300

    feat: Tarefa 01 Finalizada.

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from management import get_product_by_id, get_products_by_type
+
 if __name__ == "__main__":
     # Seus prints de teste aqui
-    ...
+    # print(get_product_by_id(28))
+    print(get_products_by_type("vegetable"))
