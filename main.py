66a422473b9ccb775dda1aef641e3d09152cdf20
Gael <rchsilver@gmail.com>
2023-05-25 19:44:24 -0300

    feat: Tarefa 03 Finalizada.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from menu import products
 
 if __name__ == "__main__":
     # Seus prints de teste aqui
-    # print(product_handler.get_product_by_id(28))
+    # print(product_handler.get_product_by_id("p"))
     # print(product_handler.get_products_by_type("vegetable"))
     # new_product: dict = {
     #     "description": "Sweet fresh strawberry on the wooden table",
@@ -15,13 +15,14 @@ if __name__ == "__main__":
     # }
     # # print(product_handler.add_product(products, **new_product))
     # print(product_handler.add_product([], **new_product))
-    table_1 = [{"_id": 1, "amount": 5}, {"_id": 19, "amount": 5}]
-    table_2 = [
-        {"_id": 10, "amount": 3},
-        {"_id": 20, "amount": 2},
-        {"_id": 21, "amount": 5},
-    ]
+    # table_1 = [{"_id": 1, "amount": 5}, {"_id": 19, "amount": 5}]
+    # table_2 = [
+    #     {"_id": 10, "amount": 3},
+    #     {"_id": 20, "amount": 2},
+    #     {"_id": 21, "amount": 5},
+    # ]
 
-    print(tab_handler.calculate_tab(table_1))
-    print(tab_handler.calculate_tab(table_2))
+    # print(tab_handler.calculate_tab(table_1))
+    # print(tab_handler.calculate_tab(table_2))
+    print(product_handler.menu_report())
     print("Fim")
--- management/product_handler.py
+++ management/product_handler.py
@@ -1,18 +1,27 @@
 from menu import products
+from collections import Counter
 
 
 def get_product_by_id(id: int):
+    if type(id) != int:
+        raise TypeError("product id must be an int")
+
     for product in products:
         if product["_id"] == id:
             return product
+
     return {}
 
 
 def get_products_by_type(search: str):
+    if type(search) != str:
+        raise TypeError("product type must be a str")
+
     search_products = []
     for product in products:
         if product["type"] == search:
             search_products.append(product)
+
     return search_products
 
 
@@ -28,4 +37,20 @@ def add_product(menu: list[dict], **kwargs: dict):
         biggest_id = max(ids)
         kwargs["_id"] = biggest_id + 1
         menu.append(kwargs)
+
         return kwargs
+
+
+def menu_report():
+    product_count = len(products)
+    total = 0.00
+    products_types = []
+
+    for product in products:
+        products_types.append(product["type"])
+        total += product["price"]
+    count_values = Counter(products_types)
+    most_common_type = count_values.most_common(1)[0][0]
+    average_price = total/product_count
+
+    return f"Products Count: {product_count} - Average Price: ${round(average_price, 2)} - Most Common Type: {most_common_type}"
